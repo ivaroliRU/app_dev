14d3f523913ec3f9ad0f9f571d45dc9f9015400e
Ívar Óli <ivars17@ru.is>
2019-11-25 20:48:32 +0000

    asdf

---
+++ Toodler/src/components/createCardButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles';
+
+class CreateCardButton extends React.Component {
+    render() {
+        /*return (
+            <view
+        );*/
+    }
+}
+
+export default CreateCardButton;
--- Toodler/src/views/MainPage/index.js
+++ Toodler/src/views/MainPage/index.js
@@ -3,28 +3,12 @@ import React from 'react';
 import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import BoardList from '../../components/boardlist';
 
-const cards = [
-  {
-    "id": 1,
-    "name": "Trip to the Netherlands!",
-    "thumbnailPhoto": "https://s25743.pcdn.co/wp-content/uploads/2018/05/points-of-interest-where-to-go-and-places-to-visit-in-the-netherlands-featured-1400x735.jpg"
-  },
-  {
-      "id": 2,
-      "name": "Boring stuff",
-      "thumbnailPhoto": "https://hbr.org/resources/images/article_assets/2019/06/Jun19_05_1040477378.jpg"
-  },
-  {
-      "id": 3,
-      "name": "Bucket list",
-      "thumbnailPhoto": "https://images-na.ssl-images-amazon.com/images/I/61fq9A8jEGL._SL1500_.jpg"
-  }
-];
+import boards from '../../../assets/data';
 
 const Main = ({ navigation: { navigate } }) => (
   <SafeAreaView style={styles.container}>
     <ScrollView style={styles.scrollView}>
-      <BoardList boards={cards} />
+      <BoardList boards={boards.boards} />
     </ScrollView>
   </SafeAreaView>
 );
