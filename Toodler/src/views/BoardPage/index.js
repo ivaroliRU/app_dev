83db3945081c0717720134f1cc191258ae0f91cb
Daníel Aron Davíðsson <danield18@ru.is>
2019-11-25 17:45:56 +0000

    hvað er í gangi?

--- Toodler/src/views/BoardPage/index.js
+++ Toodler/src/views/BoardPage/index.js
@@ -19,30 +19,3 @@ class Board extends React.Component {
 };
 
 export default Board;
-
-
-
-/*
-const Board = ({ navigation: { navigate } }) => (
-=======
-const Boards = ({ navigation: { navigate } }) => (
->>>>>>> dc5658f8eac31321bc732cd7c9d13cf0ec36b0b5:Toodler/src/views/BoardPage/index.js
-    <View style={styles.container}>
-      <Text>Board page</Text>
-    </View>
-);
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-
-<<<<<<< HEAD:Toodler/src/views/Board/index.js
-export default Board; */
-=======
-export default Boards;
->>>>>>> dc5658f8eac31321bc732cd7c9d13cf0ec36b0b5:Toodler/src/views/BoardPage/index.js
