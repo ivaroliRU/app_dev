05bb06dc3a7583b3a50c06d4959d3ed945ee12b5
Daníel Aron Davíðsson <danield18@ru.is>
2019-11-27 09:16:38 +0000

    merge

--- Toodler/src/views/MainPage/index.js
+++ Toodler/src/views/MainPage/index.js
@@ -24,7 +24,6 @@ class Main extends React.Component {
             <View style = {styles.modal}>
             <ModalContent>
                 <InputBoardName />
-                <Button title="Choose photo" onPress={this.handleChoosePhoto}></Button>
             </ModalContent>
             <ModalFooter>
                 <ModalButton
