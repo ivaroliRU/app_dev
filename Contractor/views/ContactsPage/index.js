0340f9a2c81122c5d1440ef2c2bd0712c5963143
Daníel Aron Davíðsson <danield18@ru.is>
2019-12-02 10:23:16 +0000

    added routes and views

---
+++ Contractor/views/ContactsPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+class Contacts extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <Text>
+            Hello.
+        </Text>
+    )
+  }
+};
+
+export default Contacts;
