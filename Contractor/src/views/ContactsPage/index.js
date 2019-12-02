c42174e178b64dbe0e3107abb851250850040029
Daníel Aron Davíðsson <danield18@ru.is>
2019-12-02 10:51:44 +0000

    added add contact modal

--- Contractor/src/views/ContactsPage/index.js
+++ Contractor/src/views/ContactsPage/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import AddNewContactModal from '../../components/addNewContactModal'
 
 
 class Contacts extends React.Component {
   constructor (props) {
     super(props);
+    state = {
+      modalVisible = false
+    }
+  }
+
+  handleModal = (statement) => {
+    this.setState({ modalVisible: statement });
   }
 
   render() {
     return (
-        <Text>
-            Hello.
-        </Text>
+      <View>
+        <AddNewContactModal isVisable={this.state.modalVisible} method={this.handleModal}/>
+        <Button title="Add Contact" onPress={() => this.handleModal(true)}/>
+      </View>
     )
   }
 };
