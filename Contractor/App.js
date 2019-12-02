0c0da6283aa5d088ae7a99900722488ecc537e79
Daníel Aron Davíðsson <danield18@ru.is>
2019-12-02 11:52:04 +0000

    small fixes

--- Contractor/App.js
+++ Contractor/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, navigation } from 'react-native';
 import { addContact, getAllData, cleanDirectory } from './src/services/contactsService';
 
-export default function App() {
+export default function App(navigation) {
   getAllData();
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      
-    </View>
-  );
-}
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onpress={() => {navigation.navigate('ContactPage')}}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
