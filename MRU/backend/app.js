d053a97dd23a61f1fcf79e2fdc73e91ea76cd0ce
Ívar Óli <ivars17@ru.is>
2019-12-05 13:45:39 +0000

    simple backend setup

---
+++ MRU/backend/app.js
@@ -0,0 +1,15 @@
+// api of the application
+const api = require('./api');
+
+//port the application is running on
+const PORT = process.env.PORT || 3000;
+
+//socket io connection for tcp connections
+var io = require('socket.io')(serv,{});
+
+//server for both raw tcp connections and http
+var serv = require('http').Server(api);
+
+serv.listen(PORT, () => {
+    console.log("Server running on http://localhost:" + PORT + "/");
+});
