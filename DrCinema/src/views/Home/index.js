b12bfa8c9648106b99f92f4df7eb4a8608c70fbd
Daníel Aron Davíðsson <danield18@ru.is>
2019-12-09 11:01:34 +0000

    bölvað vesen

--- DrCinema/src/views/Home/index.js
+++ DrCinema/src/views/Home/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Text, ScrollView, SafeAreaView } from 'react-native';
+import { Text, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
+
+cinemaPhoto = "http://s3-eu-west-1.amazonaws.com/fredassets/uploads/sites/30/2017/09/CinemaAccessibility.jpg"
+upcomeingPhoto = 'https://www.bollyquick.com/wp-content/uploads/2019/11/Ajay-Devgan-Upcoming-Movies.jpg'
 
 class Home extends React.Component {
   constructor (props) {
@@ -8,9 +11,10 @@ class Home extends React.Component {
 
   render() {
     return (
-      <Text style={{backgroundColor: '#E1E8EE'}}>
-          typpi
-      </Text>
+      <View style={{flex: 1, flexDirection: 'column'}}>
+      <TouchableOpacity onPress={}><Image style={{width: '50%', height: '50%'}} source={{uri: cinemaPhoto}} ></Image></TouchableOpacity>
+      <TouchableOpacity onPress={}><Image style={{width: '50%', height: '50%'}} source={{uri: upcomeingPhoto}} ></Image></TouchableOpacity>
+      </View>
     )
   }
 };
