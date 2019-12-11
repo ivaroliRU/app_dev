1037ceabb04d032a2e0f35372f184ec1d22a2b7e
Daníel Aron Davíðsson <danield18@ru.is>
2019-12-11 13:50:06 +0000

    Genres í upcoming movie list virkar nuna

--- DrCinema/src/components/upcomingMovieList/index.js
+++ DrCinema/src/components/upcomingMovieList/index.js
@@ -21,14 +21,14 @@ class UpcomingMovieList extends React.Component {
 
         return (
             <View style={{flex: 1, flexDirection: 'column', width: '100%'}}>
-                {(this.props.upcomingMovies)?this.sortMovies().reverse().map((l) => (
+                {(this.props.upcomingMovies)?this.sortMovies().map((l) => (
                     <TouchableOpacity style={{width: '100%', height: 200}} onPress={() => this.props.navigation.navigate('MovieDetails', {movie: l})} key={id++}>
                         <View style={{flex: 1, flexDirection: 'row', backgroundColor: (id%2==0)?'#111216':'#0c0d0f'}}>
                             <Image style={{width: '50%', height: '100%', resizeMode:'cover'}} source={{uri: l.poster}}></Image>
                             <View style={styles.infoContainer}>
                                 <Text  style={styles.infoHeader}>{l.title}</Text>
                                 {(l['release-dateIS'])?<Text style={styles.info}>Release date(IS): {l['release-dateIS']}</Text>:null}
-                                <Text style={styles.info}>Genres: {l.genres.map((g)=>(g + ", "))}</Text>
+                                <Text style={styles.info}>Genres: {l.genres.map((g)=>("\n" + g.Name + ", "))}</Text>
                             </View>
                         </View>
                     </TouchableOpacity>
