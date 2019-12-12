afb300b6ecd6c8609233fcb86f136d4431c8c888
Ívar Óli <ivars17@ru.is>
2019-12-12 10:56:32 +0000

    buy button pretty

--- DrCinema/src/components/BuyTicketButton/styles.js
+++ DrCinema/src/components/BuyTicketButton/styles.js
@@ -1,6 +1,24 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    buttonGContainer:{
+        borderWidth: 0,
+        height: 40,
+        backgroundColor: '#16171b',
+        justifyContent: 'center'
+    },
+    selected:{
+    },
+    button:{
+        maxWidth: 100,
+        backgroundColor: '#AAA'
+    },
+    buyButton:{
+        width: '60%',
+        marginTop: 20,
+        height: 40,
+        justifyContent: 'center'
+    }
 });
 
 export default styles;
