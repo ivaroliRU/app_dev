1e3bc8aec2b578207bacad52aedf6bd79e3b1a88
Daníel Aron Davíðsson <danield18@ru.is>
2019-12-12 10:01:39 +0000

    moving styles into styles.js

--- DrCinema/src/views/MovieDetails/index.js
+++ DrCinema/src/views/MovieDetails/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, Text, WebView, Image, Button } from 'react-native';
+import { StyleSheet, View, Text, WebView, Image, Button, SafeAreaView, ScrollView } from 'react-native';
+import styles from './style'
 import { connect } from 'react-redux';
 import BuyTicketButton from '../../components/BuyTicketButton';
 
@@ -12,36 +13,36 @@ class MovieDetails extends React.Component {
 
   render() {
     return (
-        <View style={{flex: 1, backgroundColor: '#16171b'}}>
+        <View style={styles.container}>
           {this.movie.trailers.length != 0?
             this.movie.trailers[0].results.length != 0?
-              <View style={{width: '100%', height: '40%'}}> 
+              <View style={styles.headerImageContainer}> 
                 <WebView
-                  style={{flex: 0, width: '100%', height: '100%'}}
+                  style={styles.headerWebView}
                   javaScriptEnabled={true}
                   domStorageEnabled={true}
                   source={{ uri: this.movie.trailers[0].results[0].url }}
                 />
               </View>:
-              <View style={{width: '100%', height: '40%'}}>                   
-                <Image style={{width: '100%', height: '100%', resizeMode:'cover'}} source={{uri: this.movie.poster}}></Image>
+              <View style={styles.headerImageContainer}>                   
+                <Image style={styles.headerImage} source={{uri: this.movie.poster}}></Image>
               </View>
             :
-            <View style={{width: '100%', height: '40%'}}>                   
-              <Image style={{width: '100%', height: '100%', resizeMode:'cover'}} source={{uri: this.movie.poster}}></Image>
+            <View style={styles.headerImageContainer}>                   
+              <Image style={styles.headerImage} source={{uri: this.movie.poster}}></Image>
             </View>
           }
-          <Text style={{textAlign: "center", alignContent: "center", fontSize: 20, color: '#fff', marginTop: 5}}>{this.movie.title}</Text>
-          <Text style={{textAlign: "center", alignContent: "center",color: '#fff', margin: 5}}>Release: {this.movie["release-dateIS"]}</Text>
+          <Text style={styles.textTitle}>{this.movie.title}</Text>
+          <Text style={styles.textRelease}>Release: {this.movie["release-dateIS"]}</Text>
           {this.movie.omdb.length != 0?
           <React.Fragment>
             {this.movie.hasOwnProperty('plot')?
-              <Text style={{textAlign: "center", alignContent: "center",color: '#fff', marginLeft: 20, marginRight: 20, marginTop: 5, marginBottom: 5}}>{this.movie.plot}</Text>:
-              <Text style={{textAlign: "center", alignContent: "center",color: '#fff', marginLeft: 20, marginRight: 20, marginTop: 5, marginBottom: 5}}>{this.movie.omdb.Plot}</Text>
+              <Text style={styles.textInformation}>{this.movie.plot}</Text>:
+              <Text style={styles.textInformation}>{this.movie.omdb.Plot}</Text>
             }
-            <Text style={{textAlign: "center", alignContent: "center",color: '#fff', marginLeft: 20, marginRight: 20, marginTop: 5, marginBottom: 5}}>{this.movie.omdb[0].Genre}</Text>
+            <Text style={styles.textInformation}>{this.movie.omdb[0].Genre}</Text>
           </React.Fragment>:
-        <Text style={{textAlign: "center", alignContent: "center",color: '#fff', marginLeft: 20, marginRight: 20, marginTop: 5, marginBottom: 5}}>{this.movie.plot}</Text>
+        <Text style={styles.textInformation}>{this.movie.plot}</Text>
         }
         <BuyTicketButton showtimes={this.movie.showtimes.find(element => element.cinema.id == this.cinema.id)} />
         </View>
